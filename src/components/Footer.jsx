9e4c3618bff03c43124543f2ade15995a66d4d45
AldairMC <aldairmc2023@gmail.com>
2022-05-12 02:13:24 -0500

    :sparkles: Add component Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <div className="Footer">
+            <span className="Span_Footer">
+                Desarrollado con React
+            </span>
+        </div>
+    )
+}
+
+export default Footer;
